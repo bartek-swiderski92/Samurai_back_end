7a9ffeabc9a4c645696febf7f7629f9bd5b7bd78
Phos23 <phos23@gmail.com>
2021-09-17 13:13:22 +0100

    modules

---
+++ 7-modules/app.js
@@ -0,0 +1,9 @@
+
+
+
+// const notes = require('./notes')
+// console.log('jestem w module app')
+
+// console.log(notes.txt);
+// const mod = require('module')
+// console.log(mod.wrapper);
---
+++ 7-modules/notes.js
@@ -0,0 +1,12 @@
+// console.log('jestem w module notes')
+
+// console.log('exports', exports);
+// console.log('dirname', __dirname);
+// console.log('filename', __filename);
+// console.log('require', require);
+// console.log('module', module);
+
+
+// module.exports = {
+//     txt: "Cos zwrocone z modulu notes"
+// }
