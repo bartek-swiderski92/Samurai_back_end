2ae775049754b6d37a869c6ddcca46ae8fdd88f5
Phos23 <phos23@gmail.com>
2021-09-23 18:39:34 +0100

    refactor

---
+++ Projects/TODOAPP/handleCommand.js
@@ -0,0 +1,33 @@
+const colors = require('colors')
+
+
+const handleCommand = ({
+    add,
+    remove,
+    list
+}) => {
+    // console.log(add, remove, list);
+    if (add) {
+        if (typeof add !== 'string') {
+            return console.log('wpisz nazwe dodawanego zadania (tekst)'.red)
+        } else if (add.length < 7) {
+            return console.log('Nazwa zadania musi miec wiecej niz 6 znakow.'.red)
+        }
+        handleData(1, add);
+        // console.log('bede dodawac');
+    } else if (remove) {
+        if (typeof remove !== 'string' || remove.length < 7) {
+            return console.log('Wpisz nazwe usuwanego zadania. To musi byc tekst i musi miec wiecej niz 6 znakow'.red)
+        }
+        handleData(2, remove);
+        // console.log('bede usuwac');
+    } else if (list || list === "") {
+        handleData(3, null);
+        // console.log('pokazuje liste');
+
+    } else {
+        console.log('Nie rozumiem polecenia. Użyj --add="nazwa zadania", --remove="nazwa zdania" lub opcji --list'.red);
+    }
+}
+
+module.exports = handleCommand;
