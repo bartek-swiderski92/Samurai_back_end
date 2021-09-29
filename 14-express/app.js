d53a543166141c05905c2a83234a5c9369bc7e5e
Phos23 <phos23@gmail.com>
2021-09-29 21:39:10 +0100

    Express basic setup

---
+++ 14-express/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Server is listening at http://localhost:3000');
+});
+
+app.get('/', () => {
+    console.log('Hello, World!');
+});
+
+app.get('/hi', () => {
+    console.log('Hi, World!');
+});
+
